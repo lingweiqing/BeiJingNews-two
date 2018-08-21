aa8d44fdb210de43aca528e863a4f745236fcf46
ling_leanrning <ling1471626451@Gmail.com>
2018-08-21 13:14:24 +0800

    slidingmenu库导入和侧滑菜单初步实现

--- app/src/main/java/com/example/ling/beijingnews/activity/MainActivity.java
+++ app/src/main/java/com/example/ling/beijingnews/activity/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.ling.beijingnews.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.ling.beijingnews.R;
+import com.example.ling.beijingnews.utils.DensityUtil;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //主页面
         setContentView(R.layout.activity_main);
+        //设置侧滑菜单
+        setBehindContentView(R.layout.activity_leftmenu);
+        //设置右侧菜单
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setSecondaryMenu(R.layout.activity_rightmenu);
+
+        //设置显示模式
+        slidingMenu.setMode(SlidingMenu.LEFT);
+
+        //设置滑动模式
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        //设置主页被占据的宽度
+        slidingMenu.setBehindOffset(DensityUtil.dipToPix(MainActivity.this,200));
+
     }
 }
